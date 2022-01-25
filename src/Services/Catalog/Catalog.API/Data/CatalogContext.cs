e469bb76642de48e089f607549e0e7d7b4c337bd
Yogesh Narkhede <ynarkhede@altimetrik.com>
2022-01-25 10:57:35 +0530

    Catalog.API & Docker-compose

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuraion)
+        {
+            var client = new MongoClient(configuraion.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuraion.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuraion.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
