72cca8b509c10fdd005b35faecd1b7db2651641e
Yogesh Narkhede <ynarkhede@altimetrik.com>
2022-02-09 17:30:04 +0530

    Common.Logging - Serilog Changes

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,7 +1,9 @@
+using Common.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace OcelotApiGw
 {
@@ -22,11 +24,12 @@ namespace OcelotApiGw
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .ConfigureLogging((hostingContext, loggingBuilder) =>
-                {
-                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    loggingBuilder.AddConsole();
-                    loggingBuilder.AddDebug();
-                });
+                .UseSerilog(SeriLogger.Configure);
+                //.ConfigureLogging((hostingContext, loggingBuilder) =>
+                //{
+                //    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                //    loggingBuilder.AddConsole();
+                //    loggingBuilder.AddDebug();
+                //});
     }
 }
--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,5 +1,9 @@
+using Common.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Reflection;
 
 namespace AspnetRunBasics
 {
@@ -12,6 +16,7 @@ namespace AspnetRunBasics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog(SeriLogger.Configure)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
